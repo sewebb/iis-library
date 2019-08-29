87fa4c4fd53e6f1933d37a8b16e5d171c98933d2
tbleckert <hola@tobiasbleckert.se>
2019-08-29 16:18:18 +0200

    Check if functions exists

--- src/helpers.php
+++ src/helpers.php
@@ -45,126 +45,138 @@ if ( ! function_exists( 'iis_remember' ) ) {
 	}
 }
 
-/**
- * Escape input recursively
- *
- * @param mixed $input Input parameter.
- * @return array|string
- */
-function iis_safe_get_input( $input ) {
-	if ( is_array( $input ) ) {
-		return array_map(
-			function ( $value ) {
-				return iis_safe_get_input( $value );
-			},
-			$input
-		);
-	}
-
-	return sanitize_text_field( wp_unslash( $input ) );
-}
-
-/**
- * Return a safe GET value with an optional default value.
- *
- * @param string      $key The key for the $_GET array.
- * @param null|string $default Default value if GET variable doesn't exist.
- * @return null|string
- */
-function iis_safe_get( $key, $default = null ) {
-	// Ignore nonce check for $_GET variables.
-	// phpcs:disable
-	if ( ! isset( $_GET[ $key ] ) ) {
-		return $default;
-	}
-
-	return iis_safe_get_input( $_GET[ $key ] );
-	// phpcs:enable
-}
-
-/**
- * Echo an "active"-class if the comparison is true, otherwise
- * an empty string.
- *
- * @param string $value The value to compare against.
- * @param string $compare_with The value to compare with.
- * @param string $class The class that should be echoed if true.
- * @param bool   $include_attr True if class attribute should be included.
- * @return void
- */
-function iis_active_class( $value, $compare_with = null, $class = 'is-active', $include_attr = true ) {
-	$attr = $include_attr ? 'class="' . esc_attr( $class ) . '"' : esc_attr( $class );
-
-	iis_active( $value, $compare_with, $attr, true );
-}
-
-/**
- * Echo a string if the comparison is true, otherwise empty string
- *
- * @param string       $value The value to compare against.
- * @param string|array $compare_with The value to compare with.
- * @param string       $attr The attribute that should be echoed if $echo is true.
- * @param bool         $echo True if class attribute should be included.
- * @return bool|string|void
- */
-function iis_active( $value, $compare_with = null, $attr = ' checked', $echo = true ) {
-	$value = (string) $value;
-
-	if ( is_null( $compare_with ) ) {
-		$active = ! ! $value;
-	} else {
-		$active = ( is_array( $compare_with ) ) ? in_array( $value, array_map( 'strval', $compare_with ), true ) : $value === (string) $compare_with;
-	}
-
-	if ( ! $echo ) {
-		return $active;
-	}
-
-	echo esc_html( ( $active ) ? $attr : '' );
-}
-
-/**
- * Get the laravel mix manifest
- *
- * @return array|null
- */
-function iis_mix_manifest() {
-	$mix_manifest_content = iis_remember(
-		'mix_manifest_transient',
-		1 * DAY_IN_SECONDS,
-		function () {
-			return file_get_contents( get_template_directory() . '/mix-manifest.json' );
+if ( ! function_exists( 'iis_safe_get_input' ) ) {
+	/**
+	 * Escape input recursively
+	 *
+	 * @param mixed $input Input parameter.
+	 * @return array|string
+	 */
+	function iis_safe_get_input( $input ) {
+		if ( is_array( $input ) ) {
+			return array_map(
+				function ( $value ) {
+					return iis_safe_get_input( $value );
+				},
+				$input
+			);
 		}
-	);
 
-	try {
-		$mix_manifest = json_decode( $mix_manifest_content, true );
-	} catch ( Exception $e ) {
-		$mix_manifest = null;
+		return sanitize_text_field( wp_unslash( $input ) );
 	}
-
-	return $mix_manifest;
 }
 
-/**
- * Get the path to a versioned Mix file
- *
- * @param string $path Path tp mix manifest.
- * @param string $base Base path to scripts.
- * @return string|null
- */
-function iis_mix( $path, $base = '/assets/' ) {
-	$manifest = iis_mix_manifest();
+if ( ! function_exists( 'iis_safe_get' ) ) {
+	/**
+	 * Return a safe GET value with an optional default value.
+	 *
+	 * @param string      $key The key for the $_GET array.
+	 * @param null|string $default Default value if GET variable doesn't exist.
+	 * @return null|string
+	 */
+	function iis_safe_get( $key, $default = null ) {
+		// Ignore nonce check for $_GET variables.
+		// phpcs:disable
+		if ( ! isset( $_GET[ $key ] ) ) {
+			return $default;
+		}
 
-	if ( ! $manifest ) {
-		return null;
+		return iis_safe_get_input( $_GET[ $key ] );
+		// phpcs:enable
 	}
+}
 
-	$path = $base . $path;
+if ( ! function_exists( 'iis_active_class' ) ) {
+	/**
+	 * Echo an "active"-class if the comparison is true, otherwise
+	 * an empty string.
+	 *
+	 * @param string $value The value to compare against.
+	 * @param string $compare_with The value to compare with.
+	 * @param string $class The class that should be echoed if true.
+	 * @param bool   $include_attr True if class attribute should be included.
+	 * @return void
+	 */
+	function iis_active_class( $value, $compare_with = null, $class = 'is-active', $include_attr = true ) {
+		$attr = $include_attr ? 'class="' . esc_attr( $class ) . '"' : esc_attr( $class );
 
-	if ( ! isset( $manifest[ $path ] ) ) {
-		return null;
+		iis_active( $value, $compare_with, $attr, true );
 	}
+}
 
-	return $manifest[ $path ];
+if ( ! function_exists( 'iis_active' ) ) {
+	/**
+	 * Echo a string if the comparison is true, otherwise empty string
+	 *
+	 * @param string       $value The value to compare against.
+	 * @param string|array $compare_with The value to compare with.
+	 * @param string       $attr The attribute that should be echoed if $echo is true.
+	 * @param bool         $echo True if class attribute should be included.
+	 * @return bool|string|void
+	 */
+	function iis_active( $value, $compare_with = null, $attr = ' checked', $echo = true ) {
+		$value = (string) $value;
+
+		if ( is_null( $compare_with ) ) {
+			$active = ! ! $value;
+		} else {
+			$active = ( is_array( $compare_with ) ) ? in_array( $value, array_map( 'strval', $compare_with ), true ) : $value === (string) $compare_with;
+		}
+
+		if ( ! $echo ) {
+			return $active;
+		}
+
+		echo esc_html( ( $active ) ? $attr : '' );
+	}
+}
+
+if ( ! function_exists( 'iis_mix_manifest' ) ) {
+	/**
+	 * Get the laravel mix manifest
+	 *
+	 * @return array|null
+	 */
+	function iis_mix_manifest() {
+		$mix_manifest_content = iis_remember(
+			'mix_manifest_transient',
+			1 * DAY_IN_SECONDS,
+			function () {
+				return file_get_contents( get_template_directory() . '/mix-manifest.json' );
+			}
+		);
+
+		try {
+			$mix_manifest = json_decode( $mix_manifest_content, true );
+		} catch ( Exception $e ) {
+			$mix_manifest = null;
+		}
+
+		return $mix_manifest;
+	}
+}
+
+if ( ! function_exists( 'iis_mix' ) ) {
+	/**
+	 * Get the path to a versioned Mix file
+	 *
+	 * @param string $path Path tp mix manifest.
+	 * @param string $base Base path to scripts.
+	 * @return string|null
+	 */
+	function iis_mix( $path, $base = '/assets/' ) {
+		$manifest = iis_mix_manifest();
+
+		if ( ! $manifest ) {
+			return null;
+		}
+
+		$path = $base . $path;
+
+		if ( ! isset( $manifest[ $path ] ) ) {
+			return null;
+		}
+
+		return $manifest[ $path ];
+	}
 }
