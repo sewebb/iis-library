62110ffbb9dbae8dfa60082527010498f74338ca
Tobias Bleckert <hola@tobiasbleckert.se>
2020-09-10 14:57:11 +0200

    Correct way to modify core block

--- src/blocks/core-quote.php
+++ src/blocks/core-quote.php
@@ -1,16 +1,11 @@
 <?php
 
-function iis_render_block_blockquote( $attributes, $content ) {
-	$content = str_replace( 'blockquote class="', 'blockquote class="peacock ', $content );
-	$content = str_replace( '><p>', '><svg class="iis-icon"><use xlink:href="#icon-quote"></use></svg><p>', $content );
-	$content = str_replace( '<cite>', '<cite class="meta">', $content );
+add_filter( 'render_block', function ( $content, $block ) {
+	if ( 'core/quote' === $block['blockName'] ) {
+		$content = str_replace( 'blockquote class="', 'blockquote class="peacock ', $content );
+		$content = str_replace( '><p>', '><svg class="iis-icon"><use xlink:href="#icon-quote"></use></svg><p>', $content );
+		$content = str_replace( '<cite>', '<cite class="meta">', $content );
+	}
 
 	return $content;
-}
-
-register_block_type(
-	'core/quote',
-	[
-		'render_callback' => 'iis_render_block_blockquote',
-	]
-);
+}, 10, 2 );
