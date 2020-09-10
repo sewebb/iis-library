b2285955cb14e258fe3911d4bf20311590b44c5e
Tobias Bleckert <hola@tobiasbleckert.se>
2020-09-10 14:57:49 +0200

    Lint

--- src/blocks/core-quote.php
+++ src/blocks/core-quote.php
@@ -1,11 +1,16 @@
 <?php
 
-add_filter( 'render_block', function ( $content, $block ) {
-	if ( 'core/quote' === $block['blockName'] ) {
-		$content = str_replace( 'blockquote class="', 'blockquote class="peacock ', $content );
-		$content = str_replace( '><p>', '><svg class="iis-icon"><use xlink:href="#icon-quote"></use></svg><p>', $content );
-		$content = str_replace( '<cite>', '<cite class="meta">', $content );
-	}
+add_filter(
+	'render_block',
+	function ( $content, $block ) {
+		if ( 'core/quote' === $block['blockName'] ) {
+			$content = str_replace( 'blockquote class="', 'blockquote class="peacock ', $content );
+			$content = str_replace( '><p>', '><svg class="iis-icon"><use xlink:href="#icon-quote"></use></svg><p>', $content );
+			$content = str_replace( '<cite>', '<cite class="meta">', $content );
+		}
 
-	return $content;
-}, 10, 2 );
+		return $content;
+	},
+	10,
+	2
+);
