55b2ad5b7fc5031159cefd970be8aa342d2cdbd9
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2021-10-06 10:21:59 +0200

    Added reading time helpers

--- docs/js/searchIndex.js
+++ docs/js/searchIndex.js
@@ -1,114 +1,159 @@
 Search.appendIndex(
     [
                 {
+            "fqsen": "\\Internetstiftelsen\\Byline",
+            "name": "Byline",
+            "summary": "Byline\u0020and\u0020review\u0020functionality",
+            "url": "classes/Internetstiftelsen-Byline.html"
+        },                {
+            "fqsen": "\\Internetstiftelsen\\Byline\u003A\u003AenableReview\u0028\u0029",
+            "name": "enableReview",
+            "summary": "Enable\u0020review\u0020functionality.",
+            "url": "classes/Internetstiftelsen-Byline.html#method_enableReview"
+        },                {
+            "fqsen": "\\Internetstiftelsen\\Byline\u003A\u003Arender\u0028\u0029",
+            "name": "render",
+            "summary": "Render\u0020byline",
+            "url": "classes/Internetstiftelsen-Byline.html#method_render"
+        },                {
             "fqsen": "\\iis_config\u0028\u0029",
             "name": "iis_config",
             "summary": "IIS\u0020Start\u0020config\u0020helper",
-            "url": "../namespaces/default.html#function_iis_config"
+            "url": "namespaces/default.html#function_iis_config"
         },                {
             "fqsen": "\\iis_remember\u0028\u0029",
             "name": "iis_remember",
             "summary": "Cache\u0020return\u0020value\u0020of\u0020callback\u0020if\u0020not\u0020already\u0020cached\u0020and\u0020return\u0020the\u0020contents",
-            "url": "../namespaces/default.html#function_iis_remember"
+            "url": "namespaces/default.html#function_iis_remember"
         },                {
             "fqsen": "\\iis_safe_get_input\u0028\u0029",
             "name": "iis_safe_get_input",
             "summary": "Escape\u0020input\u0020recursively",
-            "url": "../namespaces/default.html#function_iis_safe_get_input"
+            "url": "namespaces/default.html#function_iis_safe_get_input"
         },                {
             "fqsen": "\\iis_safe_get\u0028\u0029",
             "name": "iis_safe_get",
             "summary": "Return\u0020a\u0020safe\u0020GET\u0020value\u0020with\u0020an\u0020optional\u0020default\u0020value.",
-            "url": "../namespaces/default.html#function_iis_safe_get"
+            "url": "namespaces/default.html#function_iis_safe_get"
         },                {
             "fqsen": "\\iis_active_class\u0028\u0029",
             "name": "iis_active_class",
             "summary": "Echo\u0020an\u0020\u0022active\u0022\u002Dclass\u0020if\u0020the\u0020comparison\u0020is\u0020true,\u0020otherwise\nan\u0020empty\u0020string.",
-            "url": "../namespaces/default.html#function_iis_active_class"
+            "url": "namespaces/default.html#function_iis_active_class"
         },                {
             "fqsen": "\\iis_active\u0028\u0029",
             "name": "iis_active",
             "summary": "Echo\u0020a\u0020string\u0020if\u0020the\u0020comparison\u0020is\u0020true,\u0020otherwise\u0020empty\u0020string",
-            "url": "../namespaces/default.html#function_iis_active"
+            "url": "namespaces/default.html#function_iis_active"
         },                {
             "fqsen": "\\iis_mix_manifest\u0028\u0029",
             "name": "iis_mix_manifest",
             "summary": "Get\u0020the\u0020laravel\u0020mix\u0020manifest",
-            "url": "../namespaces/default.html#function_iis_mix_manifest"
+            "url": "namespaces/default.html#function_iis_mix_manifest"
         },                {
             "fqsen": "\\iis_mix\u0028\u0029",
             "name": "iis_mix",
             "summary": "Get\u0020the\u0020path\u0020to\u0020a\u0020versioned\u0020Mix\u0020file",
-            "url": "../namespaces/default.html#function_iis_mix"
+            "url": "namespaces/default.html#function_iis_mix"
         },                {
             "fqsen": "\\iis_get_hero\u0028\u0029",
             "name": "iis_get_hero",
             "summary": "Get\u0020the\u0020content\u0020hero",
-            "url": "../namespaces/default.html#function_iis_get_hero"
+            "url": "namespaces/default.html#function_iis_get_hero"
         },                {
             "fqsen": "\\iis_has_hero\u0028\u0029",
             "name": "iis_has_hero",
             "summary": "Checks\u0020if\u0020content\u0020starts\u0020with\u0020a\u0020hero",
-            "url": "../namespaces/default.html#function_iis_has_hero"
+            "url": "namespaces/default.html#function_iis_has_hero"
         },                {
             "fqsen": "\\iis_has_full_hero\u0028\u0029",
             "name": "iis_has_full_hero",
             "summary": "Checks\u0020if\u0020content\u0020starts\u0020with\u0020a\u0020full\u002Dwidth\u0020hero",
-            "url": "../namespaces/default.html#function_iis_has_full_hero"
+            "url": "namespaces/default.html#function_iis_has_full_hero"
         },                {
             "fqsen": "\\imns\u0028\u0029",
             "name": "imns",
             "summary": "Get\u0020and\u0020echo\u0020the\u0020styleguide\u0020namespace,\u0020set\u0020in\u0020.env\u002Dfile\u0020of\u0020the\u0020theme",
-            "url": "../namespaces/default.html#function_imns"
+            "url": "namespaces/default.html#function_imns"
         },                {
             "fqsen": "\\iis_uses_styleguide\u0028\u0029",
             "name": "iis_uses_styleguide",
             "summary": "Check\u0020if\u0020theme\u0020is\u0020using\u0020the\u0020styleguide",
-            "url": "../namespaces/default.html#function_iis_uses_styleguide"
+            "url": "namespaces/default.html#function_iis_uses_styleguide"
         },                {
             "fqsen": "\\iis_styleguide_sprite\u0028\u0029",
             "name": "iis_styleguide_sprite",
             "summary": "Print\u0020IIS\u0020styleguide\u0020icon\u0020sprite",
-            "url": "../namespaces/default.html#function_iis_styleguide_sprite"
+            "url": "namespaces/default.html#function_iis_styleguide_sprite"
+        },                {
+            "fqsen": "\\iis_word_count\u0028\u0029",
+            "name": "iis_word_count",
+            "summary": "Count\u0020number\u0020of\u0020words\u0020in\u0020string.",
+            "url": "namespaces/default.html#function_iis_word_count"
+        },                {
+            "fqsen": "\\iis_get_post_reading_time\u0028\u0029",
+            "name": "iis_get_post_reading_time",
+            "summary": "Get\u0020reading\u0020time\u0020for\u0020a\u0020post,\u0020in\u0020minutes.",
+            "url": "namespaces/default.html#function_iis_get_post_reading_time"
+        },                {
+            "fqsen": "\\iis_get_images_reading_time\u0028\u0029",
+            "name": "iis_get_images_reading_time",
+            "summary": "Calculate\u0020reading\u0020time\u0020for\u0020images\u0020in\u0020minutes.",
+            "url": "namespaces/default.html#function_iis_get_images_reading_time"
+        },                {
+            "fqsen": "\\Internetstiftelsen\\Submenu",
+            "name": "Submenu",
+            "summary": "Helper\u0020methods\u0020for\u0020submenu\u0020functionality",
+            "url": "classes/Internetstiftelsen-Submenu.html"
+        },                {
+            "fqsen": "\\Internetstiftelsen\\Submenu\u003A\u003Arender\u0028\u0029",
+            "name": "render",
+            "summary": "",
+            "url": "classes/Internetstiftelsen-Submenu.html#method_render"
         },                {
             "fqsen": "\\Internetstiftelsen\\Theme",
             "name": "Theme",
             "summary": "Setup\u0020common\u0020theme\u0020functionality",
-            "url": "../classes/Internetstiftelsen-Theme.html"
+            "url": "classes/Internetstiftelsen-Theme.html"
         },                {
             "fqsen": "\\Internetstiftelsen\\Theme\u003A\u003Ainit\u0028\u0029",
             "name": "init",
             "summary": "",
-            "url": "../classes/Internetstiftelsen-Theme.html#method_init"
+            "url": "classes/Internetstiftelsen-Theme.html#method_init"
         },                {
             "fqsen": "\\Internetstiftelsen\\Theme\u003A\u003Atheme_setup\u0028\u0029",
             "name": "theme_setup",
             "summary": "Setup\u0020theme",
-            "url": "../classes/Internetstiftelsen-Theme.html#method_theme_setup"
+            "url": "classes/Internetstiftelsen-Theme.html#method_theme_setup"
         },                {
             "fqsen": "\\Internetstiftelsen\\Theme\u003A\u003Aenv_banner\u0028\u0029",
             "name": "env_banner",
             "summary": "Add\u0020environment\u0020banner\u0020on\u0020dev\u0020and\u0020stage",
-            "url": "../classes/Internetstiftelsen-Theme.html#method_env_banner"
-        },                {
-            "fqsen": "\\Internetstiftelsen\\Theme\u003A\u003Asubmenu\u0028\u0029",
-            "name": "submenu",
-            "summary": "",
-            "url": "../classes/Internetstiftelsen-Theme.html#method_submenu"
+            "url": "classes/Internetstiftelsen-Theme.html#method_env_banner"
         },                {
             "fqsen": "\\Internetstiftelsen\\Theme\u003A\u003Aexclude_pingbacks\u0028\u0029",
             "name": "exclude_pingbacks",
             "summary": "Exclude\u0020xmlrpc\u0020method\u0020pingback.ping\nThis\u0020prevents\u0020other\u0020sites\u0020from\u0020inserting\u0020pingback\u0020comments\u0020into\u0020our\u0020database",
-            "url": "../classes/Internetstiftelsen-Theme.html#method_exclude_pingbacks"
+            "url": "classes/Internetstiftelsen-Theme.html#method_exclude_pingbacks"
+        },                {
+            "fqsen": "\\Internetstiftelsen\\Theme\u003A\u003Aappend_submenu_hero\u0028\u0029",
+            "name": "append_submenu_hero",
+            "summary": "",
+            "url": "classes/Internetstiftelsen-Theme.html#method_append_submenu_hero"
+        },                {
+            "fqsen": "\\Internetstiftelsen\\Theme\u003A\u003Aappend_submenu\u0028\u0029",
+            "name": "append_submenu",
+            "summary": "",
+            "url": "classes/Internetstiftelsen-Theme.html#method_append_submenu"
         },                {
             "fqsen": "\\",
             "name": "\\",
             "summary": "",
-            "url": "../namespaces/default.html"
+            "url": "namespaces/default.html"
         },                {
             "fqsen": "\\Internetstiftelsen",
             "name": "Internetstiftelsen",
             "summary": "",
-            "url": "../namespaces/internetstiftelsen.html"
+            "url": "namespaces/internetstiftelsen.html"
         }            ]
 );
