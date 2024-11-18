14850a0261cdebd46bc9009ba8b73aa32aa70bbf
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2024-11-18 12:13:45 +0100

    Only modules

--- src/helpers.php
+++ src/helpers.php
@@ -210,7 +210,7 @@ if ( ! function_exists( 'iis_vite_dev_script' ) ) {
 	 */
 	function iis_vite_dev_script(): void {
 		if ( iis_vite_is_dev() ) {
-			wp_enqueue_script( 'vite', iis_vite_dev_server_url( '@vite/client' ), [], null, true );
+			wp_enqueue_script_module( 'vite', iis_vite_dev_server_url( '@vite/client' ), [], null );
 		}
 	}
 }
@@ -302,8 +302,8 @@ if ( ! function_exists( 'iis_vite' ) ) {
 	 */
 	function iis_vite(): void {
 		if ( iis_vite_is_dev() ) {
-			wp_enqueue_script( 'vite', iis_vite_dev_server_url( '@vite/client' ), [], null, true );
-			wp_enqueue_script( 'iis-script', iis_vite_dev_server_url( 'assets/js/site.js' ), [ 'vite' ], null, true );
+			wp_enqueue_script_module( 'vite', iis_vite_dev_server_url( '@vite/client' ), [], null );
+			wp_enqueue_script_module( 'iis-script', iis_vite_dev_server_url( 'assets/js/site.js' ), [ 'vite' ], null );
 			wp_enqueue_style( 'iis-style', iis_vite_dev_server_url( 'assets/scss/site.scss' ), [], null );
 		} else {
 			$manifest = iis_vite_manifest();
