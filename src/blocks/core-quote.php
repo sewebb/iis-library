0ee8d59725a67a822c4829ae3a6e939f61be3b32
Robert Hedman <robert.hedman@internetstiftelsen.se>
2026-06-15 15:17:21 +0200

    add more logic, only add peacock class if other classes are not set

--- src/blocks/core-quote.php
+++ src/blocks/core-quote.php
@@ -4,7 +4,11 @@ add_filter(
 	'render_block',
 	function ( $content, $block ) {
 		if ( 'core/quote' === $block['blockName'] ) {
-			$content = preg_replace( '/<blockquote.*?class=\"(.*?)\".*?>/', '<blockquote class="peacock $1"><svg class="iis-icon"><use xlink:href="#icon-quote"></use></svg>', $content );
+			$content = preg_replace_callback( '/<blockquote.*?class=\"(.*?)\".*?>/', function ( $matches ) {
+				$class_name = trim( $matches[1] ) ? $matches[1] : 'peacock';
+
+				return '<blockquote class="' . $class_name . '"><svg class="iis-icon"><use xlink:href="#icon-quote"></use></svg>';
+			}, $content );
 			$content = str_replace( '<cite>', '<cite class="meta">', $content );
 		}
 
