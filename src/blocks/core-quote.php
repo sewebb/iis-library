ba43d8385ae61c3f9129257aaf1d002eecc1c599
Robert Hedman <robert.hedman@internetstiftelsen.se>
2026-06-15 14:21:02 +0200

    change class order logic

--- src/blocks/core-quote.php
+++ src/blocks/core-quote.php
@@ -4,7 +4,7 @@ add_filter(
 	'render_block',
 	function ( $content, $block ) {
 		if ( 'core/quote' === $block['blockName'] ) {
-			$content = preg_replace( '/<blockquote.*?class=\"(.*?)\".*?>/', '<blockquote class="$1 peacock"><svg class="iis-icon"><use xlink:href="#icon-quote"></use></svg>', $content );
+			$content = preg_replace( '/<blockquote.*?class=\"(.*?)\".*?>/', '<blockquote class="peacock $1"><svg class="iis-icon"><use xlink:href="#icon-quote"></use></svg>', $content );
 			$content = str_replace( '<cite>', '<cite class="meta">', $content );
 		}
 
