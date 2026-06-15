ac6ae3dd0b374c0045645d76d3e7a6e72b68ee40
Robert Hedman <robert.hedman@internetstiftelsen.se>
2026-06-15 15:21:38 +0200

    fix logic error

--- src/blocks/core-quote.php
+++ src/blocks/core-quote.php
@@ -4,8 +4,8 @@ add_filter(
 	'render_block',
 	function ( $content, $block ) {
 		if ( 'core/quote' === $block['blockName'] ) {
-			$content = preg_replace_callback( '/<blockquote.*?class=\"(.*?)\".*?>/', function ( $matches ) {
-				$class_name = trim( $matches[1] ) ? $matches[1] : 'peacock';
+			$content = preg_replace_callback( '/<blockquote(?:.*?class=\"(.*?)\")?.*?>/', function ( $matches ) {
+				$class_name = trim( $matches[1] ?? '' ) ? $matches[1] : 'peacock';
 
 				return '<blockquote class="' . $class_name . '"><svg class="iis-icon"><use xlink:href="#icon-quote"></use></svg>';
 			}, $content );
